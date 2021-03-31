cbfb21ecfb70612133f742166558ffd9ef5076fb
somadc <somaprofessional75@gmail.com>
2021-03-31 11:04:39 +0530

    Final Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React,  {Component, Fragment} from 'react';
 import './App.css';
+import Home from './Home';
+import Header from './Header';
+import Footer from './Footer';
+import SearchPage from './SearchPage';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    // BEM
+    <div className="app">
+      {/* <h1>AIRBNB CLONE🚀🚀🚀</h1> */}
+
+    <Router>
+      
+    <Header />
+
+    <Switch>
+
+      <Route path = "/search">
+        {/* SearchPage */}
+        <SearchPage />
+      </Route>
+
+      <Route path="/">
+        <Home />
+      </Route>
+
+     </Switch>
+    
+
+    <Footer />
+
+    </Router>
+
+      {/* {Home} */}
+      
+        {/* {Header} */}
+
+        {/* {Banner} */}
+          {/* {Search`} */}
+
+        {/* {Cards]} */}
+
+        {/* {Footer} */}
+
+     {/* {SearchPage} */}
+
     </div>
   );
 }
